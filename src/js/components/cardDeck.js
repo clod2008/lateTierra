56fe94c16e3e48ce04373e2e81bf5ab04460a775
clod2008 <claudio@apsis.com.ar>
2021-11-04 15:13:40 -0300

    Late tierra 0.1 icons

--- src/js/components/cardDeck.js
+++ src/js/components/cardDeck.js
@@ -7,7 +7,9 @@ export const cardDeckHome01 = () =>{
     <div class="container">
     <div class="card-deck mt-3 mb-3">
         <div class="card text-center " style="width: 18rem;">
-        <img src="holder.js/100x100" class="card-img-top align-self-center" alt="...">
+        <div class="iconCard">
+            <i class="fas fa-tractor fa-3x"></i>
+        </div>
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
